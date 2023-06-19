df6be0bf131efcdd65133da10d769955ea232f90
EnricoValente <enricovalente42@gmail.com>
2023-06-20 01:20:40 +0200

    edit array e dichiarazione variabile

---
+++ js/script.js
@@ -0,0 +1,5 @@
+const shoppingList = ['uova', 'latte', 'farina', 'pomodori', 'spinaci', 'pasta', 'riso', 'pollo', 'carote', 'salvia', 'panna', 'gelato']
+
+const listItem = document.getElementById('list-item');
+
+
