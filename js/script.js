2fd7c643ee1fb5a1dae5d0af4674011d9848e753
EnricoValente <enricovalente42@gmail.com>
2023-06-20 02:36:06 +0200

    creato ciclo while

--- js/script.js
+++ js/script.js
@@ -2,4 +2,15 @@ const shoppingList = ['uova', 'latte', 'farina', 'pomodori', 'spinaci', 'pasta',
 
 const listItem = document.getElementById('list-item');
 
+let i = 0;
+
+while(i < shoppingList.length) {
+    
+    const newLi = document.createElement('li');
+    listItem.append(newLi);
+    newLi.innerHTML = shoppingList[i];
+    i++;
+    
+    
+};
 
